5937dd3d4d25e7d9759c8905054ddb21eb5a41ab
linkwithlionhart <linkwithlionhart@gmail.com>
2024-08-20 08:11:19 -0700

    more morning practice

--- advanced-topics.py
+++ advanced-topics.py
@@ -45,3 +45,66 @@
   print my_list[::2]
 
 #9/18
+  my_list = range(1, 11)
+
+  # Add your code below!
+  backwards = my_list[::-1]
+  print backwards
+
+#10/18
+  to_one_hundred = range(101)
+  # Add your code below!
+  backwards_by_tens = to_one_hundred[::-10]
+  print backwards_by_tens
+
+#11/18
+  to_21  = range(1,22)
+  print to_21
+
+  odds = to_21[::2]
+  print odds 
+
+  middle_third = to_21[7:14:]
+  print middle_third
+
+#12/18 - anonymous functions
+  my_list = range(16)
+  print filter(lambda x: x % 3 == 0, my_list)
+
+#13/18 - Lambda functions
+  languages = ["HTML", "JavaScript", "Python", "Ruby"]
+
+  # Add arguments to the filter()
+  print filter(lambda x: x == "Python", languages)
+
+#14/18
+  squares = [x ** 2 for x in range(1,11)]
+  print squares
+
+  print filter(lambda x: x > 29 and x < 71, squares)
+
+#15/18 - iterating over dictionaries
+  movies = {
+  "Monty Python and the Holy Grail": "Great",
+  "Monty Python's Life of Brian": "Good",
+  "Monty Python's Meaning of Life": "Okay"
+}
+
+  print movies.items()
+  print movies.keys()
+  print movies.values()
+
+#16/18
+  threes_and_fives = [x for x in range(1,16) if x % 3 == 0 or x % 5 == 0]
+
+  print threes_and_fives
+
+#17/18
+  garbled = "!XeXgXaXsXsXeXmX XtXeXrXcXeXsX XeXhXtX XmXaX XI"
+  message = garbled[::-2]
+  print message
+
+#18/18
+  garbled = "IXXX aXXmX aXXXnXoXXXXXtXhXeXXXXrX sXXXXeXcXXXrXeXt mXXeXsXXXsXaXXXXXXgXeX!XX"
+  message = filter(lambda x: x != 'X', garbled)
+  print message
