bf4bc49ce815df3645880c58512164924624faf9
linkwithlionhart <linkwithlionhart@gmail.com>
2024-08-20 18:39:04 -0700

    evening practice

---
+++ intro-to-bitwise-operators.py
@@ -0,0 +1,58 @@
+#1/14
+print 5 >> 4  # Right Shift
+print 5 << 1  # Left Shift
+print 8 & 5   # Bitwise AND
+print 9 | 4   # Bitwise OR
+print 12 ^ 42 # Bitwise XOR
+print ~88     # Bitwise NOT
+
+#2/14
+print 0b1,    #1
+print 0b10,   #2
+print 0b11,   #3
+print 0b100,  #4
+print 0b101,  #5
+print 0b110,  #6
+print 0b111   #7
+print "******"
+print 0b1 + 0b11
+print 0b11 * 0b11
+
+#3/14
+one = 0b1
+two = 0b10
+three = 0b11
+four = 0b100
+five = 0b101
+six = 0b110
+seven = 0b111
+eight = 0b1000
+nine = 0b1001
+ten = 0b1010
+eleven = 0b1011
+twelve = 0b1100
+
+#4/14
+print bin(1)
+
+num_a = 2 
+while num_a < 6:
+  print bin(num_a)
+  num_a += 1 
+
+num_b = 1
+for i in range(5):
+  print bin(num_b)
+  num_b += 1
+
+#5/14
+print int("1",2)
+print int("10",2)
+print int("111",2)
+print int("0b100",2)
+print int(bin(5),2)
+# Print out the decimal equivalent of the binary 11001001.
+print int('11001001', 2)
+
+#6/14
+
