fff0be3e87d4fd536c56e516ec04fe6c5c8a76ba
linkwithlionhart <linkwithlionhart@gmail.com>
2024-08-23 20:51:53 -0700

    evening practice

---
+++ intro-to-classes.py
@@ -0,0 +1,123 @@
+#1/18
+class Fruit(object):
+  """A class that makes various tasty fruits."""
+  def __init__(self, name, color, flavor, poisonous):
+    self.name = name
+    self.color = color
+    self.flavor = flavor
+    self.poisonous = poisonous
+
+  def description(self):
+    print "I'm a %s %s and I taste %s." % (self.color, self.name, self.flavor)
+
+  def is_edible(self):
+    if not self.poisonous:
+      print "Yep! I'm edible."
+    else:
+      print "Don't eat me! I am super poisonous."
+
+lemon = Fruit("lemon", "yellow", "sour", False)
+
+lemon.description()
+lemon.is_edible()
+
+#2/18
+class Animal(object):
+  pass
+
+#3/18
+class Animal(object):
+  def __init__(self):
+    pass
+
+#4/18
+class Animal(object):
+  def __init__(self, name):
+    self.name = name
+
+#5/18
+class Animal(object):
+  def __init__(self, name):
+    self.name = name
+
+zebra = Animal("Jeffrey")
+print zebra.name
+
+#6/18
+# Class definition
+class Animal(object):
+  """Makes cute animals."""
+  # For initializing our instance objects
+  def __init__(self, name, age, is_hungry):
+    self.name = name
+    self.age = age
+    self.is_hungry = is_hungry
+
+# Note that self is only used in the __init__()
+# function definition; we don't need to pass it
+# to our instance objects.
+
+zebra = Animal("Jeffrey", 2, True)
+giraffe = Animal("Bruce", 1, False)
+panda = Animal("Chad", 7, True)
+
+print zebra.name, zebra.age, zebra.is_hungry
+print giraffe.name, giraffe.age, giraffe.is_hungry
+print panda.name, panda.age, panda.is_hungry
+
+#7/18
+class Animal(object):
+  """Makes cute animals."""
+  is_alive = True
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+
+zebra = Animal("Jeffrey", 2)
+giraffe = Animal("Bruce", 1)
+panda = Animal("Chad", 7)
+
+print zebra.name, zebra.age, zebra.is_alive
+print giraffe.name, giraffe.age, giraffe.is_alive
+print panda.name, panda.age, panda.is_alive
+
+"""""
+Lesson: Global scope. Variable is_alive is 'True' for all class instances of Animal because it is a globally available variable.
+"""""
+
+#8/18
+class Animal(object):
+  """Makes cute animals."""
+  is_alive = True
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  # Add your method here!
+  def description(self):
+    print self.name
+    print self.age
+
+hippo = Animal("Zippo", 30)
+hippo.description()
+
+#9/18
+class Animal(object):
+  """Makes cute animals."""
+  is_alive = True
+  health = "good"
+  def __init__(self, name, age):
+    self.name = name
+    self.age = age
+  # Add your method here!
+  def description(self):
+    print self.name
+    print self.age
+
+hippo = Animal("Zippo", 30)
+hippo.description()
+sloth = Animal("Slouch", 18)
+ocelot = Animal("Jag", 21)
+
+print hippo.health
+print sloth.health
+print ocelot.health
