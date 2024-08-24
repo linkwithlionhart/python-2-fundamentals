0a655f514ef8a602247aaa5da6c932a06b08b827
linkwithlionhart <linkwithlionhart@gmail.com>
2024-08-24 10:20:53 -0700

    morning practice

--- intro-to-classes.py
+++ intro-to-classes.py
@@ -121,3 +121,158 @@ ocelot = Animal("Jag", 21)
 print hippo.health
 print sloth.health
 print ocelot.health
+
+#10/18
+class ShoppingCart(object):
+  """Creates shopping cart objects
+  for users of our fine website."""
+  
+  def __init__(self, customer_name):
+    self.customer_name = customer_name
+    self.items_in_cart = {}
+  def add_item(self, product, price):
+    """Add product to the cart."""
+    if not product in self.items_in_cart:
+      self.items_in_cart[product] = price
+      print product + " added."
+    else:
+      print product + " is already in the cart."
+
+  def remove_item(self, product):
+    """Remove product from the cart."""
+    if product in self.items_in_cart:
+      del self.items_in_cart[product]
+      print product + " removed."
+    else:
+      print product + " is not in the cart."
+
+my_cart = ShoppingCart('Will')
+my_cart.add_item('Eggs', 12.00)
+print my_cart.customer_name
+
+#11/18
+class Customer(object):
+  """Produces objects that represent customers."""
+  def __init__(self, customer_id):
+    self.customer_id = customer_id
+
+  def display_cart(self):
+    print "I'm a string that stands in for the contents of your shopping cart!"
+
+class ReturningCustomer(Customer):
+  """For customers of the repeat variety."""
+  def display_order_history(self):
+    print "I'm a string that stands in for your order history!"
+
+monty_python = ReturningCustomer("ID: 12345")
+monty_python.display_cart()
+monty_python.display_order_history()
+
+#12/18
+class Shape(object):
+  """Makes shapes!"""
+  def __init__(self, number_of_sides):
+    self.number_of_sides = number_of_sides
+
+# Add your Triangle class below!
+class Triangle(Shape):
+  def __init__(self, side1, side2, side3):
+    self.side1 = side1
+    self.side2 = side2
+    self.side3 = side3
+
+#13/18
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
+
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 12.00
+
+new_employee = PartTimeEmployee("Will")
+print new_employee.employee_namea
+print new_employee.calculate_wage(37.5)
+
+"""
+Lesson: properly instantiate the correct object by calling the right keyword. 'class' not 'def'.
+"""
+
+#14/18
+class Employee(object):
+  """Models real-life employees!"""
+  def __init__(self, employee_name):
+    self.employee_name = employee_name
+
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 20.00
+
+# Add your code below!
+class PartTimeEmployee(Employee):
+  def calculate_wage(self, hours):
+    self.hours = hours
+    return hours * 12.00
+  
+  def full_time_wage(self, hours):
+    return super(PartTimeEmployee, self).calculate_wage(hours)
+
+milton = PartTimeEmployee("Milton")
+print milton.full_time_wage(10.0)
+
+#15/18
+class Triangle(object):
+  def __init__(self, angle1, angle2, angle3):
+    self.angle1 = angle1
+    self.angle2 = angle2
+    self.angle3 = angle3
+
+#16/18
+class Triangle(object):
+  def __init__(self, angle1, angle2, angle3):
+    self.angle1 = angle1
+    self.angle2 = angle2
+    self.angle3 = angle3
+  
+  number_of_sides = 3
+  
+  def check_angles(self):
+    if (self.angle1 + self.angle2 + self.angle3) == 180:
+      return True
+    else:
+      return False
+
+#17/18
+class Triangle(object):
+  number_of_sides = 3
+  
+  def __init__(self, angle1, angle2, angle3):
+    self.angle1 = angle1
+    self.angle2 = angle2
+    self.angle3 = angle3
+    
+  def check_angles(self):
+    if (self.angle1 + self.angle2 + self.angle3) == 180:
+      return True
+    else:
+      return False
+  
+my_triangle = Triangle(90, 30, 60)
+print my_triangle.number_of_sides
+print my_triangle.check_angles()
+
+#18/18
+class Equilateral(Triangle):
+  angle = 60
+
+  def __init__(self):
+    self.angle1 = self.angle 
+    self.angle2 = self.angle
+    self.angle3 = self.angle
